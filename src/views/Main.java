4bd77a907b5b4ad84e8b916841e8cb7046076395
= <patohru2006@gmail.com>
2025-06-08 10:46:02 +0700

    feat: add menu and options

---
+++ src/views/Main.java
@@ -0,0 +1,28 @@
+import Utils.Utils;
+public class Main {
+	public static void main(String args[]) throws IOException {
+		int options;
+		boolean cont = true;
+		UserView userView = new UserView();
+
+		do {
+			System.out.println(
+				"1. Create User Account\n" +
+				"2. Check exist user\n" +
+				"3. Search user infomation by name\n" + 
+				"4. Update user\n" +
+				"5. Delete user\n" +
+				"6. Save account to file\n" + 
+				"7. Print list user from file\n" +
+				"8. exit\n"
+			);
+
+			options = Utils.getInt("Enter option: ", 1, 8);
+
+			switch(options) {
+				case 1:
+					break;
+			}
+		} while(true && cont);
+	}
+}
